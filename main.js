3d1f1956d6ff853749deceaee5c146f659116d94
Shrinikawhitehatjr <97172285+Shrinikawhitehatjr@users.noreply.github.com>
2022-07-25 19:40:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+img = "";
+status = "";
+objects = [];
+
+function setup(){
+    canvas = createCanvas(380, 380);
+    canavs.center();
+    video.createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+}
+function start(){
+
+    objectDetector = ml5.objectDetector(video, modalLoaded);
+    document.getElementById("status").innerHTML = "Status : OBJECT DETECTED";
+}
+function draw(){
+    img(image, 0, 0, 380, 380);
+
+    if(status !="")
+    {
+     r = random(255);
+     g = random(255);
+     b = random(255);
+     objectDetector.detect(video, gotResults);
+     for(i = 0; i < objects.length; i++);
+     {
+        document.getElementById("status").innerHTML = "Status : OBJECT DETECTED";
+
+        document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ objects.length;
+
+            fill(r,g,b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y +15);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+     }
+     function preload(){
+        Audio(my_baby.mp3);
+     }
